4fe36b4e9c204d2d21e7d4ea4127cdb9a36b9ce9
zacktram <zacktram@gmail.com>
2024-03-31 11:45:27 -0400

    changed deployment url for socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin:["http://localhost:3000"],
+        origin:["https://chatroom-xvu6.onrender.com"],
         methods: ["GET", "POST"],
     },
 });
