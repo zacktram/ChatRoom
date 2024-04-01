e41a3ce7de8e1b50141b1213f05ca7b45acd09c0
zacktram <zacktram@gmail.com>
2024-04-01 19:43:11 -0400

    updated socket url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin:["https://chatroom-xvu6.onrender.com"],
+        origin:["http://localhost:3000"],
         methods: ["GET", "POST"],
     },
 });
